06bdd4c4cfb59fc295f89211ebaa2f5be88c0d07
sherbet-limon <davydova0909@gmail.com>
2025-01-31 19:38:04 +0300

    add func Add

--- parcel.go
+++ parcel.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
 )
 
 type ParcelStore struct {
@@ -12,15 +15,29 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 	return ParcelStore{db: db}
 }
 
-func (s ParcelStore) Add(p Parcel) (int, error) {
+func (s ParcelStore) Add(p Parcel) (int64, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
-
+res, err := s.db.Exec("INSERT INTO parsel (Number, Client, Status, Address, CreatedAt) VALUES (:Number, :Client, :Status, :Address, :CreatedAt)",
+	sql.Named("Number", p.Number),
+	sql.Named("Client", p.Client),
+	sql.Named("Status", p.Status),
+	sql.Named("Address", p.Address),
+	sql.Named("CreatedAt", p.CreatedAt))
+	if err != nil {
+        fmt.Println(err)
+        return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+	return id, nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
 	// реализуйте чтение строки по заданному number
+
 	// здесь из таблицы должна вернуться только одна строка
 
 	// заполните объект Parcel данными из таблицы
